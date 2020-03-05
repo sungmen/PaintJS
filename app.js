07f44dd33de26ece2bf7d3003d67320d12aaeb30
SungMen Joen <spe0678@gmail.com>
2020-03-05 09:55:39 +0900

    an updated commit

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-ss
+const canvas = document.getElementById("jsCanvas");
+function onMouseMove(event) {
+    const x = event.offsetX;
+    const y = event.offsetY;
+    
+}
+
+if(canvas) {
+    canvas.addEventListener("mousemove", onMouseMove);
+}
