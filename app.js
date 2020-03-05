e96deaf0b195b8738e97ba5f006c712f4d9d1c75
sungmen <spe0678@gmail.com>
2020-03-05 12:51:52 +0900

    update

--- app.js
+++ app.js
@@ -1,4 +1,11 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
+
+canvas.width = 700;
+canvas.height = 700;
 
 let painting = false;
 
@@ -6,22 +13,25 @@ function stopPainting() {
 	painting = false;
 }
 
-function onMouseMove(event) {
-    const x = event.offsetX;
-    const y = event.offsetY;
-}
-
-function onMouseDown(event) {
+function startPainting() {
 	painting = true;
 }
 
-function onMouseUp(event) {
-	stopPainting();
+function onMouseMove(event) {
+    const x = event.offsetX;
+    const y = event.offsetY;
+	if(!painting) {
+		ctx.beginPath();
+		ctx.moveTo(x+92, y+92);
+	} else {
+		ctx.lineTo(x+92, y+92);
+		ctx.stroke();
+	}
 }
 
 if(canvas) {
 	canvas.addEventListener("mousemove", onMouseMove);
-	canvas.addEventListener("mousedown", onMouseDown);
-	canvas.addEventListener("mouseup", onMouseUp);
+	canvas.addEventListener("mousedown", startPainting);
+	canvas.addEventListener("mouseup", stopPainting);
 	canvas.addEventListener("mouseleave", stopPainting);
 }
